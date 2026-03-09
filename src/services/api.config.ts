59ff6ab8522355117ea23ae0ca47f7a6ea0b12f6
mamadou-abass-diallo-sre <m.abassdiallo@gmail.con>
2026-03-09 15:29:22 +0000

    change the build url for axios

--- src/services/api.config.ts
+++ src/services/api.config.ts
@@ -1,5 +1,5 @@
 export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080/api";
+  process.env.NEXT_PUBLIC_API_URL || "https://spare-api.symmetry.sn/api";
 
 export function mockDelay(ms = 500): Promise<void> {
   return new Promise((resolve) => setTimeout(resolve, ms));
